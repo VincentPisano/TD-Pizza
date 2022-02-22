688d918c420d86f021b12c97e7be78ce929ea68c
Vincent Pisano <vincentpisano@live.fr>
2022-02-22 09:19:07 +0100

    Exercice 1 Fini

---
+++ carte_pizzeria.py
@@ -0,0 +1,25 @@
+class CartePizzeria:
+    
+    carte = []
+    
+    def __init__():
+        pass
+    
+    def is_empty(self):
+        if len(self.carte) == 0:
+            return True
+        return False 
+        
+    
+    def nb_pizzas(self):
+        return len(self.carte)
+        
+    def add_pizza(self, pizza):
+        self.carte.append(pizza)
+            
+    
+    def remove_pizza(self, name):
+        try : 
+            self.carte.remove(name) 
+        except  ValueError : 
+              print("La pizza que vous souhaitez enlever de la carte n'existe pas !")
